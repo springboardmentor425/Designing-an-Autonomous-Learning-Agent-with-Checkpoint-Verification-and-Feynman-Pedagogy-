e1d24eec12bf6dae4caee7294577a1582eee5c68
Dharsh016 <dharshuram2004@gmail.com>
2026-01-01 20:04:49 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+
+
+load_dotenv()
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+)
+
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI assistant. Answer clearly."),
+    ("human", "{input}")
+])
+
+chain = prompt_template | model
+
+
+print("Gemini Chatbot (type 'exit' to stop)\n")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() in ["exit", "quit"]:
+        print("👋 Chat ended.")
+        break
+
+    response = chain.invoke({"input": user_input})
+    print(f"AI: {response.content}\n")
