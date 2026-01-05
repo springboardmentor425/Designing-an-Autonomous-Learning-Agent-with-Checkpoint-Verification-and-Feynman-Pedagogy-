e013fb1d722b426a67cf87fa8fec81b82d42f123
Rayana Karthikeyan <rayanakarthikeyan@gmail.com>
2026-01-05 17:00:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.0-pro",  
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.5
+)
+
+response = model.invoke("Explain REST API in simple words.")
+print(response.content)
