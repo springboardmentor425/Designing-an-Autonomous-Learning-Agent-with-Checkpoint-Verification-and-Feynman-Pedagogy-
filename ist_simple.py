d242dc505543f344f8619b0e823a5116b367954c
Shivani3333 <codercpp1@gmail.com>
2026-01-05 16:47:05 +0530

    langchain use

--- ist_simple.py
+++ ist_simple.py
@@ -1,22 +1,36 @@
 import os
 from dotenv import load_dotenv
-from google import genai
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+
 
 load_dotenv()
+os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-client = genai.Client(api_key=GOOGLE_API_KEY)
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7,
+)
+
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful AI assistant. Answer clearly."),
+    ("human", "{input}")
+])
+
+chain = prompt_template | model
+
+
+print("Gemini Chatbot (type 'quit' to stop)\n")
 
 while True:
-    prompt = input("You: ").strip()
-    if prompt.lower() in ["exit", "quit"]:
+    user_input = input("You: ")
+
+    if user_input.lower() in ["exit", "quit"]:
+        print("Chat ended.")
         break
-    if not prompt:
-        continue
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
-    )
-
-    print("Gemini:", response.text)
+    response = chain.invoke({"input": user_input})
+    print(f"AI: {response.content}\n")
