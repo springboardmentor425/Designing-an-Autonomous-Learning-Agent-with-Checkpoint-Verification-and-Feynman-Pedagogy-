aff59861ff48bf5d9162d3666826aa22049bdf93
Vsreecharan <sreecharan2311@gmail.com>
2026-01-06 16:36:30 +0530

    Initial work by Sreecharan

--- main.py
+++ main.py
@@ -1,28 +1,10 @@
 import os
-from dotenv import load_dotenv
-from openai import OpenAI
+from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
+import dotenv
+dotenv.load_dotenv()
 
-load_dotenv()
+model=init_chat_model("google_genai:models/gemini-2.5-flash")
 
-client = OpenAI(
-    api_key=os.getenv("GROQ_API_KEY"),
-    base_url="https://api.groq.com/openai/v1"
-)
-
-
-print("Chatbot is ready! Type 'exit' to quit.")
-
-# Chat loop
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        print("Goodbye!")
-        break
-    try:
-        response = client.chat.completions.create(
-            model="llama-3.1-8b-instant",
-            messages=[{"role": "user", "content": user_input}]
-        )
-        print("Bot:", response.choices[0].message.content)
-    except Exception as e:
-        print("Error:", str(e))
+result=model.invoke([{"role":"user", "content": "write a poem about Rain."}])
+print(result.content)
