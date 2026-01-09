50f3790fca538044f2eefe9b5cd1e89251b2f85e
Saniya <nirmalesaniya@gmail.com>
2026-01-09 20:28:02 +0530

    Added Tavily helper functions

--- Main.py
+++ Main.py
@@ -4,8 +4,9 @@ import dotenv
 dotenv.load_dotenv()
 
 
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
-result = model.invoke("Explain Feynman Technique in simple words.")
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+
+result = model.invoke("What is date today?")
 
 print(result.content)
